c818bbfcd6062e6b1e1b0d91af0334761f1d26e7
Ryan Mc Donald <S00199690@mail.itsligo.ie>
2021-03-25 23:40:09 +0000

    bootstrap fix

--- src/app/services/omdb-api.service.ts
+++ src/app/services/omdb-api.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
-import {HttpClient, HttpErrorResponse } from "@angular/common/http";
-import { Observable} from 'rxjs';
-import { catchError,tap } from 'rxjs/operators';
+import { HttpClient, HttpErrorResponse } from "@angular/common/http";
+import { Observable } from 'rxjs';
+import { catchError, tap } from 'rxjs/operators';
 import { IOMDBResponse } from '../omdbresponse';
 
 
@@ -10,21 +10,21 @@ import { IOMDBResponse } from '../omdbresponse';
 })
 export class OmdbApiService {
 
-  private _siteURL="https://www.omdbapi.com/";
-  private _key='?apikey=c31ae126&t=';
-  constructor(private _http:HttpClient) { }
+  private _siteURL = "https://www.omdbapi.com/";
+  private _key = '?apikey=c31ae126&t=';
+  constructor(private _http: HttpClient) { }
 
   getMovieData(movieName): Observable<IOMDBResponse> {
     return this._http.get<IOMDBResponse>(this._siteURL + this._key + movieName)
-    .pipe(
-      tap(data => console.log('Moviedata/error' + JSON.stringify(data))
-    ),
-    catchError(this.handleError)
-    );
+      .pipe(
+        tap(data => console.log('Moviedata/error' + JSON.stringify(data))
+        ),
+        catchError(this.handleError)
+      );
 
   }
 
-  private handleError(err:HttpErrorResponse) {
+  private handleError(err: HttpErrorResponse) {
     console.log('OmdbApiService: ' + err.message);
     return Observable.throw(err.message);
   }
