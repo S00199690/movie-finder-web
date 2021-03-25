e0927b22c2285a8d8800035910adc2ea87360771
Ryan Mc Donald <S00199690@mail.itsligo.ie>
2021-03-25 02:04:22 +0000

    Movie Details display

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
 import { Component } from '@angular/core';
+import { OmdbApiService } from './services/omdb-api.service';
+import { IOMDBResponse } from './omdbresponse';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [OmdbApiService]
+
 })
 export class AppComponent {
-  title = 'movie-finder';
-}
+  
+  movieData:IOMDBResponse;
+  errorMessage:any;
+
+  constructor(private _ombdbService:OmdbApiService) {
+
+  }
+
+  getMovieDetails(movieName:string) :boolean {
+    this._ombdbService.getMovieData(movieName).subscribe(
+      movieData => {
+        this.movieData=this.movieData;
+        console.log('Director name: ' + this.movieData.Director);
+      },
+      error => this.errorMessage = <any>error
+      );
+      return false;
+
+    }
+  }
+
